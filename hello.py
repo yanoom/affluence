94da9210a7f785663dfed2a748040b443e366d78
yanoom <yanoom@gmail.com>
2018-08-29 09:16:10 +0300

    Added Sum in show(), query is now in a function and 'db' is global

--- hello.py
+++ hello.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 
 app_language = "Hebrew" # "English"
 
+db = MySQLdb.connect(host="localhost",  # your host
+                     user="root",  # username
+                     passwd="mySqlDb6",  # password
+                     db="import_schema")  # name of the database
+
+def deb_execute_query(db, query, commit = False):
+    # Create a Cursor object to execute queries.
+    cur = db.cursor()
+
+    # Select data from table using SQL query.
+    try:
+        cur.execute(query)
+        if (commit):
+            db.commit()
+    except:
+        if (commit):
+            db.rollback()
+        return "Error occured: " + query
+    return cur
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -14,46 +34,27 @@ def hello():
 def show():
     result = ""
 
-    db = MySQLdb.connect(host="localhost",  # your host
-                     user="root",  # username
-                     passwd="mySqlDb6",  # password
-                     db="import_schema")  # name of the database
-
-    # Create a Cursor object to execute queries.
-    cur = db.cursor()
-
     # Select data from table using SQL query.
     select_query = "SELECT * FROM client_test" if request.args.get('showall') else "SELECT * FROM client_test WHERE deleted = 0"
-    try:
-        cur.execute(select_query)
-    except:
-        return "Error occured: " + select_query
+    cur = deb_execute_query(db, select_query)
 
     # print the first and second columns
     for row in cur.fetchall():
         result += "<p>" + str(row[0]) + " " + row[1] + "</p>"
+
+    sum_query = "SELECT SUM(data) FROM import_schema.client_test;"
+    cur = deb_execute_query(db, sum_query)
+    # print the first column
+    for row in cur.fetchall():
+        result += "<strong>" + str(row[0]) + "</strong>"
 
     db.close()
     return result
 
 @app.route("/add/", methods=["GET", "POST"])
 def add():
-
-    db = MySQLdb.connect(host="localhost",  # your host
-                     user="root",  # username
-                     passwd="mySqlDb6",  # password
-                     db="import_schema")  # name of the database
-
-    # Create a Cursor object to execute queries.
-    cur = db.cursor()
-
     insert_query = "INSERT INTO `import_schema`.`client_test` (`data`) VALUES ('" + request.args.get('num') + "');"
-    try:
-        cur.execute(insert_query)
-        db.commit()
-    except:
-        db.rollback()
-        return "Error occured:" + insert_query
+    deb_execute_query(db, insert_query)
 
     db.close()
     return show()
@@ -61,21 +62,8 @@ def add():
 @app.route("/hard_remove/", methods=["GET", "POST"])
 def hard_remove():
 
-    db = MySQLdb.connect(host="localhost",  # your host
-                     user="root",  # username
-                     passwd="mySqlDb6",  # password
-                     db="import_schema")  # name of the database
-
-    # Create a Cursor object to execute queries.
-    cur = db.cursor()
-
     delete_query = "DELETE FROM `import_schema`.`client_test` WHERE `idclient_test` = " + request.args.get('id') + ";"
-    try:
-        cur.execute(delete_query)
-        db.commit()
-    except:
-        db.rollback()
-        return "Error occured:" + delete_query
+    deb_execute_query(db, delete_query, True)
 
     db.close()
     return show()
@@ -83,21 +71,8 @@ def hard_remove():
 @app.route("/soft_delete/", methods=["GET", "POST"])
 def soft_delete():
 
-    db = MySQLdb.connect(host="localhost",  # your host
-                     user="root",  # username
-                     passwd="mySqlDb6",  # password
-                     db="import_schema")  # name of the database
-
-    # Create a Cursor object to execute queries.
-    cur = db.cursor()
-
     update_query = "UPDATE`import_schema`.`client_test` SET `deleted` = 1 WHERE `idclient_test` = " + request.args.get('id') + ";"
-    try:
-        cur.execute(update_query)
-        db.commit()
-    except:
-        db.rollback()
-        return "Error occured:" + update_query
+    deb_execute_query(db, update_query, True)
 
     db.close()
     return show()
